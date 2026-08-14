f58c8a90ca20206ccf33bf770cdba5750d840a2b
= <kamisaberi@gmail.com>
2026-08-14 15:52:11 +0330

    some codes finished

---
+++ src/ai/ai_manager.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "blackbox/event.hpp"
+#include <xinfer/xinfer.hpp>
+#include <memory>
+#include <string>
+
+namespace blackbox::ai {
+
+class AIManager {
+public:
+    AIManager(xinfer::Target target, const std::string& model_path);
+    ~AIManager() = default;
+
+    // Evaluates security event features using xInfer Essential Engine
+    float analyze_event(SecurityEvent& event);
+
+private:
+    std::unique_ptr<xinfer::Engine> xinfer_engine_;
+    bool is_initialized_{false};
+};
+
+} // namespace blackbox::ai
