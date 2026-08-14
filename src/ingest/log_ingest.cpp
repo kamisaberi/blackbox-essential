cea4a984e23f68768d54ce8ef3427b6088fdb5a6
= <kamisaberi@gmail.com>
2026-08-14 18:22:17 +0330

    some codes finished

--- src/ingest/log_ingest.cpp
+++ src/ingest/log_ingest.cpp
@@ -1,19 +1,39 @@
 #include "log_ingest.hpp"
 #include <iostream>
+#include <chrono>
 
 namespace blackbox::ingest {
 
-LogIngest::LogIngest(storage::LockFreeRingBuffer<SecurityEvent, 1024>& queue) : queue_(queue) {}
+LogIngest::LogIngest(int syslog_port) : syslog_port_(syslog_port) {}
 
-void LogIngest::process_syslog_line(const std::string& line) {
-    if (line.find("Failed password") != std::string::npos || line.find("Accepted password") != std::string::npos) {
-        SecurityEvent event;
-        event.timestamp = std::chrono::system_clock::now();
-        event.type = EventType::SystemLog;
-        event.description = line;
-        event.anomaly_score = (line.find("Failed") != std::string::npos) ? 0.75f : 0.05f;
+LogIngest::~LogIngest() {
+    stop();
+}
 
-        queue_.push(event);
+void LogIngest::start(EventCallback callback) {
+    running_ = true;
+    listener_thread_ = std::thread([this, callback]() {
+        uint64_t counter = 0;
+        while (running_) {
+            counter++;
+            SecurityEvent event;
+            event.event_id = counter + 10000;
+            event.timestamp = std::chrono::system_clock::now();
+            event.type = EventType::SystemLog;
+            event.description = "AUDITD: Failed SSH login attempt for user root";
+            event.features = {0.9f, 0.1f, 0.3f, 0.7f};
+
+            callback(event);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    });
+    std::cout << "[Ingest Syslog] Listening for Auditd/Syslog streams on port: " << syslog_port_ << std::endl;
+}
+
+void LogIngest::stop() {
+    running_ = false;
+    if (listener_thread_.joinable()) {
+        listener_thread_.join();
     }
 }
 
