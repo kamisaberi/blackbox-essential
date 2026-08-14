5f628feabe43e6ee09a3f3ff7b5ad4fb0dbc9188
= <kamisaberi@gmail.com>
2026-08-14 15:52:55 +0330

    some codes finished

---
+++ src/mitigation/ebpf_blocker.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <unordered_set>
+#include <mutex>
+
+namespace blackbox::mitigation {
+
+class EBPFBlocker {
+public:
+    EBPFBlocker() = default;
+    ~EBPFBlocker() = default;
+
+    // Drops packets from malicious IP at kernel level
+    bool block_ip(const std::string& ip_address);
+    bool unblock_ip(const std::string& ip_address);
+
+private:
+    std::unordered_set<std::string> blocked_ips_;
+    std::mutex blocker_mutex_;
+};
+
+} // namespace blackbox::mitigation
