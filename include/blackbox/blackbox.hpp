4fd713ecf6ca0cc66701e5035122505b36169e6c
= <kamisaberi@gmail.com>
2026-08-15 04:21:15 +0330

    some codes finished

--- include/blackbox/blackbox.hpp
+++ include/blackbox/blackbox.hpp
@@ -3,6 +3,7 @@
 #include "blackbox/export.hpp"
 #include "blackbox/types.hpp"
 #include "blackbox/event.hpp"
+#include "blackbox/engine_instance.hpp"
 #include "blackbox/correlator.hpp"
 #include "blackbox/mitigation.hpp"
 #include "blackbox/storage.hpp"
