0adc53070b8f3dce7746e0c9c0836ce43a6fa73b
kamran saberifard <kamisaberi@gmail.com>
2026-09-04 21:33:40 +0330

    xdp_drop.c created

--- scripts/build_ebpf.sh
+++ scripts/build_ebpf.sh
@@ -1,6 +1,22 @@
+# #!/usr/bin/env bash
+# set -e
+
+# echo "Compiling eBPF Kernel C code into BPF bytecode..."
+# clang -O2 -target bpf -c src/mitigation/xdp_drop.c -o build/xdp_drop.o
+# echo "eBPF bytecode successfully compiled to build/xdp_drop.o"
+
+
+
 #!/usr/bin/env bash
 set -e
 
-echo "Compiling eBPF Kernel C code into BPF bytecode..."
-clang -O2 -target bpf -c src/mitigation/xdp_drop.c -o build/xdp_drop.o
-echo "eBPF bytecode successfully compiled to build/xdp_drop.o"
+ARCH=$(uname -m)
+echo "Compiling eBPF XDP kernel bytecode for architecture: ${ARCH}..."
+
+clang -O2 -target bpf \
+      -I/usr/include/${ARCH}-linux-gnu \
+      -I/usr/include \
+      -c src/mitigation/xdp_drop.c \
+      -o src/mitigation/xdp_drop.o
+
+echo "eBPF bytecode successfully compiled to src/mitigation/xdp_drop.o"
